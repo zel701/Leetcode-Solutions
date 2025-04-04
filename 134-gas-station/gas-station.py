3a36622082e8f8187e8914e19ee0081d0556278d
zel701 <158342004+zel701@users.noreply.github.com>
2025-04-04 00:18:58 -0700

    Time: 131 ms (5.04%) | Memory: 24.1 MB (6.61%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,10 +1,8 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         profit = []
-        gasleft = [0]
         for i in range(len(gas)):
             profit.append(gas[i] - cost[i])
-
         x = -1
         j = 0
         gasremain = 0
@@ -30,22 +28,12 @@ class Solution:
                     print(gasremain)
                     Tripstart = True
             else:
-
                 gasremain = gasremain + profit[x]
-
-                print(gasremain)
                 if x == j:
                     break
                 if gasremain < 0:
                     print(gasremain)
                     Tripstart = False
                     j=-1
+        return j
 
-        for i in range(len(profit)):
-            if i>0:
-                gasleft.append(gasleft[i-1] + profit[i])
-
-        for i in range(len(gasleft)):
-            if gasleft[i] >= 0:
-                return j
-        return -1
